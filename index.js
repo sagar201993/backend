36bf0ad329b9f8679bf6d9f477e415b16a66a983
Munish Sagar <munishsagar@Munishs-MacBook-Air.local>
2024-02-17 14:54:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+const port = 3000;
+import Android from "./categories/Android.json" assert { type: "json" };
+import Apple from "./categories/Apple.json" assert { type: "json" };
+import Cooling from "./categories/cooling.json" assert { type: "json" };
+
+app.use(express.json());
+
+app.get("/smartphones", (req, res) => {
+  res.json({
+    data: Android,
+  });
+  console.log(data);
+});
+
+app.get("/apple", (req, res) => {
+  res.json({
+    data: Apple,
+  });
+});
+
+app.get("/cooling", (req, res) => {
+  res.json({
+    data: Cooling,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
